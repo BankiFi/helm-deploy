97b271389c5b7300891fe191042112ee7bed7a24
alonsodomin <antonio.dominguez@bankifi.com>
2021-12-20 12:57:58 +0100

    Make args mutable

--- src/helm.ts
+++ src/helm.ts
@@ -9,7 +9,7 @@ export async function addRepository(
   username?: string,
   password?: string
 ): Promise<void> {
-  const args = ['repo', 'add', alias, url]
+  var args = ['repo', 'add', alias, url]
 
   if (username) args.push(`--username=${username}`)
   if (password) args.push(`--password=${password}`)
